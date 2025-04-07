fb9c7c6cebce2307035ab80b76514cfa46fbba18
Jerry Febriano <febrianojerry@gmail.com>
2025-04-07 13:35:07 +0700

    fix: remove item

--- NgiritKuy/NgiritKuyApp.swift
+++ NgiritKuy/NgiritKuyApp.swift
@@ -5,19 +5,19 @@
 //  Created by Jerry Febriano on 07/04/25.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct NgiritKuyApp: App {
     var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        let schema = Schema([])
+        let modelConfiguration = ModelConfiguration(
+            schema: schema, isStoredInMemoryOnly: false)
 
         do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+            return try ModelContainer(
+                for: schema, configurations: [modelConfiguration])
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
         }
